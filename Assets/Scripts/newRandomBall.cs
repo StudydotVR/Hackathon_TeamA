5a45f93df495645b1bdf8db9d60567987b6941b2
milk333 <USER1@16-C17.local>
2016-06-29 19:29:26 +0900

    音楽、壁作りました。

--- Assets/Scripts/newRandomBall.cs
+++ Assets/Scripts/newRandomBall.cs
@@ -5,16 +5,16 @@ public class newRandomBall : MonoBehaviour {
 	private int variety;//玉の種類
 	// Use this for initialization
 	void Start () {
-		
+		variety = 5;
 		selectBall ();
 
 	}
 	public	int selectBall(){
-		int number = Random.Range (1, variety);
+		int number = Random.Range (0, variety);
 		return number;
 	}
 	// Update is called once per frame
 	void Update () {
-		selectBall ();
+		
 	}
 }
