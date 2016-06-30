573bb38ba2f9b4c1d060a1d422eab1b174d6e538
milk333 <USER1@16-C17.local>
2016-06-30 21:24:27 +0900

    外観変更。（天井、床、壁に区分）　警告イベント（画面とSE変更）

---
+++ Assets/Scripts/Keikoku.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Keikoku : MonoBehaviour {
+
+	public Material[]  materials; 
+	private AudioSource warning03;
+	public AudioClip sound;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer> ().material = materials [0];
+		warning03 = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Teki") {
+			GetComponent<Renderer> ().material = materials [1];
+			warning03.PlayOneShot(sound);
+		}
+	}
+
+	void OnTriggerExit(Collider collider){
+				if (collider.gameObject.tag == "Teki") {
+			GetComponent<Renderer>().material = materials[0]; 
+		}
+	}
+
+}
