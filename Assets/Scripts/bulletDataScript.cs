467ebff2e00776c7ae760c79759cb2fc6b84c023
池上綾乃 <ikegami.timecapsule@gmail.com>
2016-07-02 18:38:03 +0900

    配列の実装

--- Assets/Scripts/bulletDataScript.cs
+++ Assets/Scripts/bulletDataScript.cs
@@ -2,14 +2,41 @@
 using System.Collections;
 
 public class bulletDataScript : MonoBehaviour {
-
+	private string[] chemicalFormulaArray;
+	private int[] tagArray;
 	// Use this for initialization
 	void Start () {
-	
+		chemicalFormulaArray = new string[] {
+			"HCl",
+			"HNO",
+			"HSO",
+			"CHCOOH",
+			"HCN",
+			"COOH",
+			"HS",
+			"NAOH",
+			"KOH",
+			"CaOH",
+			"BaOH",
+			"NH",
+			"CuOH",
+			"MgOH",
+			"FeOH"
+		};
+		tagArray = new int[] {
+			-3, -3, -3, -2, -2, -2, -2, -2, 3, 3, 3,3, 2, 2, 2, 2
+		};
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	public int[] getTagArray(){
+		return tagArray;
+	}
+
+	public string[] getchemicalArray(){
+		return chemicalFormulaArray;
 	}
 }
