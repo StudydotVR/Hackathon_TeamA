5eba3fec06bbd486eeecc6aa240803122e076214
池上綾乃 <ikegami.timecapsule@gmail.com>
2016-07-02 18:26:50 +0900

    ラベル付け

---
+++ Assets/Scripts/titleButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class titleButtonScript : MonoBehaviour {
+	static string leveltag;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void OnClick(){
+		leveltag = gameObject.tag;
+		SceneManager.LoadScene ("TScene");
+
+	}
+}
